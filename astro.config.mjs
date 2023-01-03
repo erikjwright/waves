64a979add7a870502a71aec7730159d4084c5565
Erik Wright <wright.erikj@gmail.com>
2023-01-03 08:08:39 +0100

    add tailwind, update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.waves-trading.com',
+  integrations: [tailwind(), sitemap(), prefetch()]
+});
